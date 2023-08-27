efa0514117aae0ae54e0c19b733530953cf7dd48
fcamadi <fcamadi@gmail.com>
2023-08-27 21:18:24 +0200

    feat: add default input data

--- src/main/java/_01_intro/WordCount.java
+++ src/main/java/_01_intro/WordCount.java
@@ -1,5 +1,6 @@
 package _01_intro;
 
+import _01_intro.data.Data;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
@@ -19,13 +20,19 @@ public class WordCount
         // Make parameters available in the web interface
         env.getConfig().setGlobalJobParameters(params);
 
+        DataSet<String> text = null;
         // Read the text file from the given input path
-        DataSet<String> text = env.readTextFile(params.get("input"));
-
+        if (params.has("input")) {
+            text = env.readTextFile(params.get("input"));
+            System.out.println("input:" + params.get("input"));
+        } else {
+            System.out.println("Executing WordCount example with default input data set.");
+            System.out.println("Use --input to specify file input.");
+            text = Data.getDefaultTextLineDataSet(env);
+        }
         DataSet<String> filtered = text.filter(new FilterFunction<String>() {
-            public boolean filter(String value)
-            {
-                return value.startsWith("N");
+            public boolean filter(String value) {
+                return value.startsWith("T");
             }
         });
 
@@ -33,15 +40,16 @@ public class WordCount
         DataSet<Tuple2<String, Integer>> tokenized = filtered.map(new Tokenizer());
 
         // Group by the tuple field 0, and sum up tuple field 1
-        DataSet<Tuple2<String, Integer>> counts = tokenized.groupBy(new int[] { 0 }).sum(1);
+        DataSet<Tuple2<String, Integer>> counts = tokenized.groupBy(new int[]{0}).sum(1);
 
         // Emit result
-        if (params.has("output"))
-        {
+        if (params.has("output")) {
             counts.writeAsCsv(params.get("output"), "\n", " ");
-
             // Execute program
-            env.execute("WordCount Example");
+            env.execute("WordCount Example - Words starting with 'T'");
+        } else {
+            System.out.println("Printing result to stdout. Use --output to specify output path.");
+            counts.print();
         }
     }
 
