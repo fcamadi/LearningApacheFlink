37b90985a3bf8572ccfa3f8d5a77f6c0ae3371f1
francd <fcamadi@gmail.com>
2023-09-13 00:28:56 +0200

    feat: add parameter "start", to customize the search of words

--- src/main/java/_01_intro/WordCount.java
+++ src/main/java/_01_intro/WordCount.java
@@ -8,10 +8,15 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 
+import java.io.Serializable;
+
 public class WordCount
 {
+
     public static void main(String[] args) throws Exception
     {
+        String START = "A"; // default starting character to look for
+
         // Set up execution environment
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
@@ -21,6 +26,15 @@ public class WordCount
         env.getConfig().setGlobalJobParameters(params);
 
         DataSet<String> text = null;
+
+        if (params.has("start")) {
+            START = params.get("start");
+            System.out.println("Searching for words starting with: "+START);
+        }
+        else {
+            System.out.println("Searching for words starting with default character: "+START);
+        }
+
         // Read the text file from the given input path
         if (params.has("input")) {
             text = env.readTextFile(params.get("input"));
@@ -30,11 +44,8 @@ public class WordCount
             System.out.println("Use --input to specify file input.");
             text = Data.getDefaultTextLineDataSet(env);
         }
-        DataSet<String> filtered = text.filter(new FilterFunction<String>() {
-            public boolean filter(String value) {
-                return value.startsWith("T");
-            }
-        });
+
+        DataSet<String> filtered = text.filter(getFilterFunction(START));
 
         // Split up the lines in pairs <word, 1>
         DataSet<Tuple2<String, Integer>> tokenized = filtered.map(new Tokenizer());
@@ -46,12 +57,17 @@ public class WordCount
         if (params.has("output")) {
             counts.writeAsCsv(params.get("output"), "\n", " ");
             // Execute program
-            env.execute("WordCount Example - Words starting with 'T'");
+            env.execute("WordCount Example - Words starting with "+START);
         } else {
             System.out.println("Printing result to stdout. Use --output to specify output path.");
             counts.print();
         }
     }
+
+    public static FilterFunction<String> getFilterFunction(String start)  {
+        return s -> s.startsWith(start);
+    }
+
 
     public static final class Tokenizer implements MapFunction<String, Tuple2<String, Integer>>
     {
